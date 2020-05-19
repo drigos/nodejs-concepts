aec9befa3271d75a090a490585bd57121b72d509
Rodrigo F Martins <martins.drg@gmail.com>
2020-05-19 15:11:03 -0300

    refactor: create routes file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-import { uuid } from 'uuidv4';
-
+import routes from './routes';
 import {
   traceErrors,
   errorHandler,
@@ -10,10 +9,6 @@ import {
   internalServerErrorHandler,
   malformedInputErrorHandler,
 } from './middleware';
-
-import { NotUniqueError } from './errors';
-import findDuplicatedKeys from './lib/findDuplicatedKeys';
-import createRepositoryValidator from './validators/create-repository';
 
 const app = express();
 
@@ -24,51 +19,7 @@ const app = express();
 app.use(jsonResponseFormatter);
 app.use(cors());
 app.use(express.json());
-
-const repositories = [];
-
-app.get("/repositories", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories", (request, response) => {
-  const repositoryData = createRepositoryValidator(request.body);
-
-  const subjects = findDuplicatedKeys(repositories, repositoryData, [
-    'title',
-    'url',
-  ]);
-
-  if (subjects.length) {
-    throw new NotUniqueError('Repository already registered', { subjects });
-  }
-
-  // TODO: verify whether `repositoryData.url` exists in GitHub API
-
-  const repository = {
-    ...repositoryData,
-    id: uuid(),
-    likes: 0,
-  };
-  repositories.push(repository);
-
-  response
-    .status(201)
-    .location(`/repositories/${repository.id}`)
-    .json(repository);
-});
-
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.use(routes);
 
 //
 // Error handlers:
---
+++ src/routes.js
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+
+import { uuid } from 'uuidv4';
+
+import { NotUniqueError } from './errors';
+import findDuplicatedKeys from './lib/findDuplicatedKeys';
+import createRepositoryValidator from './validators/create-repository';
+
+const routes = new Router();
+
+const repositories = [];
+
+routes.get("/repositories", (request, response) => {
+  // TODO
+});
+
+routes.post("/repositories", (request, response) => {
+  const repositoryData = createRepositoryValidator(request.body);
+
+  const subjects = findDuplicatedKeys(repositories, repositoryData, [
+    'title',
+    'url',
+  ]);
+
+  if (subjects.length) {
+    throw new NotUniqueError('Repository already registered', { subjects });
+  }
+
+  // TODO: verify whether `repositoryData.url` exists in GitHub API
+
+  const repository = {
+    ...repositoryData,
+    id: uuid(),
+    likes: 0,
+  };
+  repositories.push(repository);
+
+  response
+    .status(201)
+    .location(`/repositories/${repository.id}`)
+    .json(repository);
+});
+
+routes.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+routes.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+routes.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+export default routes;
